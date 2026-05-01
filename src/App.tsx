1aa0b10153a4951143a41f77d9828a28531d0c82
= <=>
2026-04-30 23:47:40 -0300

    "refactoring"

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,22 @@ import { Box } from "@mui/material";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import MainContent from "./components/MainContent/MainContent";
+import { Outlet } from "react-router";
 
 const container = {
-  display: "grid",
-  gridTemplateAreas: `
-          "header header"
-          "aside content"
-        `,
-  gridTemplateColumns: "10% 90%",
-  gridTemplateRows: "7% 93%",
+  display: "flex",
+  flexDirection: "column",
   height: "100%",
-  justifyContent: "center",
+  justifyContent: "space-between",
+  alignItems: "strech",
+  backgroundColor: "#ffffff",
 };
 
 export default function App() {
   return (
     <Box sx={container}>
       <Header />
-      <NavBar />
-      <MainContent />
+      <Outlet />
     </Box>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,22 @@ import App from "./App.tsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme/theme.ts";
 import "./assets/index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Login from "./pages/Login.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider theme={theme}>
-    <StrictMode>
-      <CssBaseline />
-      <App />
-    </StrictMode>
+    <BrowserRouter>
+      <StrictMode>
+        <CssBaseline />
+        <Routes>
+          <Route element={<App />}>
+            <Route index path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </StrictMode>
+    </BrowserRouter>
   </ThemeProvider>,
 );
