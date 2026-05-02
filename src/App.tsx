b36793a698942f74d1e4f8a6753b467abbf01237
= <=>
2026-05-02 11:02:48 -0300

    "refactoring: removendo importações não utilizadas"

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import Header from "./components/Header/Header";
 import NavBar from "./components/NavBar/NavBar";
 import MainContent from "./components/MainContent/MainContent";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 const container = {
   display: "flex",
