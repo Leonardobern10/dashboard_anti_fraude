c60a8d69335cbb01b62523c903079b1e3636501f
= <=>
2026-05-02 11:01:01 -0300

    "refactoring"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,6 @@
-import { Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import logo from "/logo.svg";
+import avatar from "../../assets/img/user_avatar.png";
 
 export default function Header() {
   return (
@@ -7,12 +9,29 @@ export default function Header() {
       sx={{
         height: "8vh",
         boxShadow: "2px 0px 5px #c5c5c5",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingX: 8,
       }}
       component="header"
     >
-      <Typography>Hoje</Typography>
-      <Typography>Hoje</Typography>
-      <Typography>Hoje</Typography>
+      <Box>
+        <Box>
+          <img src={logo} loading="lazy" alt="logo" />
+        </Box>
+      </Box>
+      <Stack
+        direction="row"
+        spacing={4}
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingRight: 3,
+        }}
+      >
+        <img src={avatar} loading="lazy" alt="user image" />
+        <Typography variant="body2">Admin</Typography>
+      </Stack>
     </Stack>
   );
 }
